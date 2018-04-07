0377a5328e18cc51886aa8a7d5a61db414691325
MariamAbdullah <m.abdullah@messtechnik.at>
2018-04-07 13:47:07 +0200

    gradle

--- app/src/main/java/com/example/mabdullah/timetracingapp/DBManager.java
+++ app/src/main/java/com/example/mabdullah/timetracingapp/DBManager.java
@@ -15,8 +15,9 @@ import java.util.ArrayList;
 public class DBManager extends SQLiteOpenHelper {
     public static final String DBName="TimeManager.db";
     private static final String User_TABLE_CREATE = "create table User (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)";
-    private static final String Project_TABLE_CREATE = "create table Project (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, Description TEXT)";
+    private static final String Project_TABLE_CREATE = "create table Project (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT)";
     private static final String Time_TABLE_CREATE = "create table Time (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, von datetime default current_timestamp, bis datetime )";
+    private static final String CrossTable_TABLE_CREATE = "create table CrossTable (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER REFERENCES User(id), projectId INTEGER REFERENCES Project(id), timeId INTEGER REFERENCES TIme(id) )";
 
 
 
@@ -61,6 +62,25 @@ public class DBManager extends SQLiteOpenHelper {
         User user = new User(Integer.parseInt(cursor.getString(0)), cursor.getString(1),cursor.getString(2));
         return user;
     }
+ /*   public  ArrayList <Project> getProjects(){
+        ArrayList<Project> projectList= new ArrayList<>();
+        SQLiteDatabase db= this.getReadableDatabase();
+        String projectlist= "select * from Project";
+        Cursor cursor= db.rawQuery(projectlist, null);
+        cursor.moveToFirst();
+        if (cursor != null) {
+            // Loop through all Results
+            do {
+                String ProjectId = cursor.getString(id);
+                String projectName = cursor.getString(name);
+            }while(c.moveToNext());
+
+        *//*Cursor cursor= db.query("Project", new String[]{"id","name","description"},"id"+ "=?",
+                new String[]{String.valueOf(id)},null,null,null,null);
+
+        projectList.add(cursor);*//*
+        return projectList;
+    }*/
 
     /*public List<User> getAllUsers(){
         List<User> userList=new ArrayList<>();
